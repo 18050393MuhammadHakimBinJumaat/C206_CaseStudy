0dab90af415baaf0bfc987ba02712e6c1cca3e09
19014273 <19014273@CHENG-PC>
2020-08-18 14:26:35 +0800

    update

---
+++ C206_CaseStudy/src/ManageCustomerMain.java
@@ -0,0 +1,19 @@
+/**
+ * I declare that this code was written by me
+  * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+ * 19014273, 18 Aug 2020 2:24:30 pm
+
+ */
+
+/**
+ * I declare that this code was written by me.
+ *I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+*19014273, 18 Aug 2020 2:24:30 pm
+ */
+public class ManageCustomerMain {
+
+}
