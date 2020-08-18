955b890070de984c7da0f0df61372d7aa6267782
18050393 <18050393@PFT-18050393.rp.edu.sg>
2020-08-18 11:36:09 +0800

    I create ManagePackageTest, ManageQuotationTes and ManageRequestquotatio

--- C206_CaseStudy/test/ManageRequestquotationTest.java
+++ C206_CaseStudy/test/ManageRequestquotationTest.java
@@ -1,12 +1,10 @@
-
 /**
  * I declare that this code was written by me.
  *I will not copy or allow others to copy my code.
  * I understand that copying code is considered as plagiarism.
 *
-*18050393, 18 Aug 2020 11:28:10 am
+*18050393, 18 Aug 2020 11:34:10 am
  */
-
-public class ManagePackage {
+public class ManageRequestquotationTest {
 
 }
