ac2fc53d907b3d48956faf85f2aaaf296b5ab7fd
19027922 <19027922@DESKTOP-GRACLMI>
2020-08-18 11:43:07 +0800

    ManageAppointmentTestClass

---
+++ C206_CaseStudy/test/ManageAppointmentTest.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19027922, 18 Aug 2020 11:42:44 am
+ */
+
+public class ManageAppointmentTest {
+
+}
