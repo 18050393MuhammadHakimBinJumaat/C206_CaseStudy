d9586c63883d6893e7e2e5eb131a47aedd07b35b
19027922 <19027922@DESKTOP-GRACLMI>
2020-08-18 23:19:23 +0800

    Manage Appointment complete

--- C206_CaseStudy/src/ManageAppointment.java
+++ C206_CaseStudy/src/ManageAppointment.java
@@ -13,6 +13,54 @@ import java.time.LocalDateTime;
 public class ManageAppointment {
 	 private String customerName;
      private String designerName;
-     private LocalDateTime appointmentDate;
+     private LocalDateTime appointmentTime;
+     private LocalDate appointmentDate;
+     
+     
+     public ManageAppointment(String customerName, String designerName, LocalDate appointmentDate, LocalDateTime appointmentTime) {
+     this.customerName = customerName;
+     this.designerName = designerName;
+     this.appointmentDate = appointmentDate;
+     this.appointmentTime = appointmentTime;
+     }
+     public String 
+     getcustomerName() {
+    	 return customerName;
+     }
+     public String
+     getdesignerName() {
+    	 return designerName;
+     }
+     public LocalDateTime 
+    	 getappointmentTime() {
+    	 return appointmentTime;
+     }
+     public LocalDate
+        getappointmentDate() {
+    	 return appointmentDate;
+     }
+     public void
+     setCustomerName(String customerName) {
+    	 this.customerName = customerName;
+     }
+     public void
+     setDesignerName(String DesignerName) {
+    	 this.designerName = designerName;
+     }
+     public void
+     setAppointmentDate(LocalDate AppointmentDate) {
+    	 this.appointmentDate = appointmentDate;
+     }
+     public void
+     setAppointmentTime(LocalDateTime AppointmentTime) {
+    	 this.appointmentTime = appointmentTime;
+     }
+     public void 
+     showDetailsof() {
+    	  System.out.println("The customer: "+ customerName);
+    	  System.out.println("has an appointment on :"+ appointmentDate);
+    	  System.out.println("Appointment Time:"+ appointmentTime);
+    	  System.out.println("With the designer:" + designerName);
+     }
 }
 
