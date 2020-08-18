1a3a94e0ff1961550e03d3da873640e1740a574e
19014273 <19014273@CHENG-PC>
2020-08-18 11:29:48 +0800

    JUnit

---
+++ C206_CaseStudy/test/ManageCustomerTest.java
@@ -0,0 +1,19 @@
+/**
+ * I declare that this code was written by me
+  * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+ * 19014273, 18 Aug 2020 11:29:40 am
+
+ */
+
+/**
+ * I declare that this code was written by me.
+ *I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+*19014273, 18 Aug 2020 11:29:40 am
+ */
+public class ManageCustomerTest {
+
+}
