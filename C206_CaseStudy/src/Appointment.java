e6b856e835319f2675540056736c85fe3ef4dfb9
19027922 <19027922@DESKTOP-GRACLMI>
2020-08-20 21:27:47 +0800

    AppointmentClass

---
+++ C206_CaseStudy/src/Appointment.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19027922, 20 Aug 2020 9:27:07 pm
+ */
+
+public class Appointment {
+
+}
