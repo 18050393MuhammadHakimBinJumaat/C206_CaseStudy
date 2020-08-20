71156e6fcde20d000d8637c36b157a2b4a07d4f1
19027922 <19027922@DESKTOP-GRACLMI>
2020-08-20 21:28:08 +0800

    salma

--- C206_CaseStudy/src/Appointment.java
+++ C206_CaseStudy/src/Appointment.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * I declare that this code was written by me.
@@ -8,5 +10,59 @@
  */
 
 public class Appointment {
+	public class ManageAppointment {
+		 private String customerName;
+	     private String designerName;
+	     private LocalDateTime appointmentTime;
+	     private LocalDate appointmentDate;
+	     
+	     
+	     public ManageAppointment(String customerName, String designerName, LocalDate appointmentDate, LocalDateTime appointmentTime) {
+	     this.customerName = customerName;
+	     this.designerName = designerName;
+	     this.appointmentDate = appointmentDate;
+	     this.appointmentTime = appointmentTime;
+	     }
+	     public String 
+	     getcustomerName() {
+	    	 return customerName;
+	     }
+	     public String
+	     getdesignerName() {
+	    	 return designerName;
+	     }
+	     public LocalDateTime 
+	    	 getappointmentTime() {
+	    	 return appointmentTime;
+	     }
+	     public LocalDate
+	        getappointmentDate() {
+	    	 return appointmentDate;
+	     }
+	     public void
+	     setCustomerName(String customerName) {
+	    	 this.customerName = customerName;
+	     }
+	     public void
+	     setDesignerName(String DesignerName) {
+	    	 this.designerName = designerName;
+	     }
+	     public void
+	     setAppointmentDate(LocalDate AppointmentDate) {
+	    	 this.appointmentDate = appointmentDate;
+	     }
+	     public void
+	     setAppointmentTime(LocalDateTime AppointmentTime) {
+	    	 this.appointmentTime = appointmentTime;
+	     }
+	     public void 
+	     showDetailsof() {
+	    	  System.out.println("The customer: "+ customerName);
+	    	  System.out.println("has an appointment on :"+ appointmentDate);
+	    	  System.out.println("Appointment Time:"+ appointmentTime);
+	    	  System.out.println("With the designer:" + designerName);
+	     }//salma
+	}
+
 
 }
