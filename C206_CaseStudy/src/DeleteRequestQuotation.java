acdbc812ee477035ca79b5a40984446b1574ecc6
19014273 <19014273@CHENG-PC>
2020-08-23 00:08:21 +0800

    Updated 5

--- C206_CaseStudy/src/DeleteRequestQuotation.java
+++ C206_CaseStudy/src/DeleteRequestQuotation.java
@@ -3,7 +3,7 @@
   * I will not copy or allow others to copy my code.
  * I understand that copying code is considered as plagiarism.
 *
- * 19014273, 22 Aug 2020 8:03:00 pm
+ * 19014273, 23 Aug 2020 12:08:06 am
 
  */
 
@@ -12,10 +12,9 @@
  *I will not copy or allow others to copy my code.
  * I understand that copying code is considered as plagiarism.
 *
-*19014273, 22 Aug 2020 8:03:00 pm
+*19014273, 23 Aug 2020 12:08:06 am
  */
-//ChengHan
-public class DeleteQuotation {
+public class DeleteRequestQuotation {
 
 	/**
 	 * @param args
