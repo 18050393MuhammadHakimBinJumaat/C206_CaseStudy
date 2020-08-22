ba5ea0764ff8f4d89461a926b2e12ce22d36b3bf
19014273 <19014273@CHENG-PC>
2020-08-22 20:07:49 +0800

    Final update

--- C206_CaseStudy/src/DeleteQuotation.java
+++ C206_CaseStudy/src/DeleteQuotation.java
@@ -3,7 +3,7 @@
   * I will not copy or allow others to copy my code.
  * I understand that copying code is considered as plagiarism.
 *
- * 19014273, 22 Aug 2020 7:58:38 pm
+ * 19014273, 22 Aug 2020 8:03:00 pm
 
  */
 
@@ -12,8 +12,16 @@
  *I will not copy or allow others to copy my code.
  * I understand that copying code is considered as plagiarism.
 *
-*19014273, 22 Aug 2020 7:58:38 pm
+*19014273, 22 Aug 2020 8:03:00 pm
  */
-public class RequestQuotation {
+public class DeleteQuotation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
