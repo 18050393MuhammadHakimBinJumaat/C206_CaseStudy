7febcb728029132a3bfe7a2b15cccaaffc8ac39f
19014273 <19014273@CHENG-PC>
2020-08-23 12:01:58 +0800

    ViewReqQuotat

---
+++ C206_CaseStudy/src/ViewRequestQuotation.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+/**
+ * I declare that this code was written by me
+  * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+ * 19014273, 23 Aug 2020 11:55:40 am
+
+ */
+
+/**
+ * I declare that this code was written by me.
+ *I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+*19014273, 23 Aug 2020 11:55:40 am
+ */
+
+//Cheng han
+public class ViewRequestQuotation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ArrayList<RequestQuotation> QuotList= new ArrayList<RequestQuotation>();
+		
+		String output = String.format("%-15s %-10s %-10s %-20s %-10s %-15s %-15s %-15s %-15s \n", "PROPERTY TYPE",
+				"AREA SIZE", "CONTACT", "EMAIL", "BUDGET", "COMPLETE DATE", "RENO TYPE", "RENO STYLE", "URGET");
+		System.out.println(output);
+	}
+
+		// TODO Auto-generated method stub
+
+	
+}
