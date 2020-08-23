4f020d636a2fb7852600eac962a46bc75a862c1a
19014273 <19014273@CHENG-PC>
2020-08-23 12:05:28 +0800

    DD

--- C206_CaseStudy/src/DeleteRequestQuotation.java
+++ C206_CaseStudy/src/DeleteRequestQuotation.java
@@ -1,19 +1,4 @@
-/**
- * I declare that this code was written by me
-  * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
-*
- * 19014273, 23 Aug 2020 12:08:06 am
 
- */
-
-/**
- * I declare that this code was written by me.
- *I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
-*
-*19014273, 23 Aug 2020 12:08:06 am
- */
 
 //Cheng Han
 public class DeleteRequestQuotation {
