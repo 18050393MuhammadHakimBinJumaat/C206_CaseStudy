34deb22cbe0da3517f4da07dc304e033f4dd714c
19014273 <19014273@CHENG-PC>
2020-08-23 12:48:17 +0800

    Junit

---
+++ C206_CaseStudy/test/DeleteRequestQuotation.java
@@ -0,0 +1,20 @@
+/**
+ * I declare that this code was written by me
+  * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+ * 19014273, 23 Aug 2020 12:47:31 pm
+
+ */
+
+/**
+ * I declare that this code was written by me.
+ *I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+*
+*19014273, 23 Aug 2020 12:47:31 pm
+ */
+//Cheng Han
+public class DeleteRequestQuotation {
+
+}
