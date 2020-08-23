c0c95688bf3daa3b17f03d08bf5122ce922ebfa1
19027922 <19027922@DESKTOP-GRACLMI>
2020-08-24 03:16:14 +0800

    UpdateAppointment

--- C206_CaseStudy/src/UpdateAppointment.java
+++ C206_CaseStudy/src/UpdateAppointment.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 /**
  * I declare that this code was written by me.
@@ -14,7 +15,22 @@ public class UpdateAppointment {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Appointment> AppointmentList1 = new ArrayList<Appointment>();
+		String CustomerName = Helper.readString("Enter name of Customer whose appointment is to be updated > ");
+		boolean found = false;
+		for (int i = 0; i < AppointmentList1.size(); i++) {
+			if (CustomerName.equalsIgnoreCase(AppointmentList1.get(i).getName())) {
+				String designerName = Helper.readString("Enter new designer name > ");
+				AppointmentList1.get(i).setCustomerName(CustomerName);
+				AppointmentList1.get(i).setDesignerName(designerName);
+				found = true;
+			}
+		}
+		if (found == false) {
+			System.out.println("Customer with the name has no appointment");
+		} else {
+			System.out.println("Appointment successfully Updated");
+		}
 	}
 
 }
