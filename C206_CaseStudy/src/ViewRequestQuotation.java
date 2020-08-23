26f826d3f7788c5979c8f7dcba096b6bfd8b8636
19014273 <19014273@CHENG-PC>
2020-08-23 12:05:11 +0800

    D

--- C206_CaseStudy/src/ViewRequestQuotation.java
+++ C206_CaseStudy/src/ViewRequestQuotation.java
@@ -1,21 +1,6 @@
 import java.util.ArrayList;
 
-/**
- * I declare that this code was written by me
-  * I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
-*
- * 19014273, 23 Aug 2020 11:55:40 am
 
- */
-
-/**
- * I declare that this code was written by me.
- *I will not copy or allow others to copy my code.
- * I understand that copying code is considered as plagiarism.
-*
-*19014273, 23 Aug 2020 11:55:40 am
- */
 
 //Cheng han
 public class ViewRequestQuotation {
